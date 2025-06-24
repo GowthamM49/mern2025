d030bb5d537e1584ffcc2ec38a2b83221eadc043
Gowtham <gowthamm.23it@kongu.edu>
2025-06-24 09:11:22 +0530

    day6

--- Backend/server.js
+++ Backend/server.js
@@ -9,5 +9,5 @@ connectDB();
 app.use(express.json());
 app.use(router);
 app.listen(PORT, () => {
-    console.log(`Server is running on port http://localhost:${PORT}`);
+    console.log(`Server is a running on port http://localhost:${PORT}`);
 })
