0239ebfe294c0354f590658c3d07cf546150eb5c
Gowtham <gowthamm.23it@kongu.edu>
2025-06-25 11:38:42 +0530

    last 1

--- react/src/components/Todo.jsx
+++ react/src/components/Todo.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-const API='http://localhost:3000';
+const API='https://mern2025-ujl5.onrender.com';
 
 const Todo = () => {
   const [task, setTask] = useState('');
